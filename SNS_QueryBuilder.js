7cee571fe916dc0a79e1187e9741d279edccc929
Allen Linatoc <linatoc.allen@gmail.com>
2016-12-24 22:51:17 +0800

    Update SNS_QueryBuilder.js

--- SNS_QueryBuilder.js
+++ SNS_QueryBuilder.js
@@ -1,6 +1,32 @@
 var SNS_QueryBuilder = Class.create();
+var SNS_QueryObject = function() {
+    
+};
+
 SNS_QueryBuilder.prototype = {
+    
+    // ALLEN: Properties
+    //
+    _stack: [ ],
+    
+    
+    // ALLEN: Methods
+    //
+    
     initialize: function() {
+        
+    },
+    
+    begin: function(fieldname) {
+        
+    },
+    
+    add: function(field) {
+        
+    },
+    
+    addObject: function(object) {
+        
     },
 
     type: 'SNS_QueryBuilder'
